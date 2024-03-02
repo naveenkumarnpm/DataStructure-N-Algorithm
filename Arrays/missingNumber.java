aacfea432aa2c13845dfc28aa8a268d5c519c752
naveenkumar <naveenkumarnb76@gmail.com>
2024-03-02 11:37:54 +0530

    missing number in array

---
+++ Arrays/missingNumber.java
@@ -0,0 +1,16 @@
+public class missingNumber {
+    public static void main(String[] args) {
+        int arr[]={1,2,3,4,5,7}; //Need not to be sorted
+        int n=arr.length+1;
+        int sum=n*(n+1)/2;
+        int arrSum=0;
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            arrSum+=arr[i];
+        }
+
+        int missingNum=sum-arrSum;
+        System.out.println(missingNum);
+    }
+}
