ae813d8bc374c36587167dd0aab31a6c767fce4d
naveenkumar <naveenkumarnb76@gmail.com>
2024-03-05 19:22:15 +0530

    incomplete

---
+++ Arrays/railwayPlatform.java
@@ -0,0 +1,16 @@
+//Incomplete--
+
+public class railwayPlatform {
+    public static void main(String[] args) {
+        int arival[]={100, 140, 150, 200, 215, 400};
+        int depeture[]={110, 300, 220, 230, 315, 600};
+
+        for(int i=0;i<arival.length;i++)
+        {
+            if(arival[i]<depeture[i])
+            {
+
+            }
+        }
+    }
+}
