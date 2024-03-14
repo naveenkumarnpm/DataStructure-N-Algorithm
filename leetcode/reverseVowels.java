ae7f641869e5c079d10bcf40e7026060597f7c8f
naveenkumar <naveenkumarnb76@gmail.com>
2024-03-14 08:58:48 +0530

    Day-5 | reverse a vowel within the Words or Sentence

---
+++ leetcode/reverseVowels.java
@@ -0,0 +1,35 @@
+package leetcode;
+
+public class reverseVowels {
+    public String reverseVowels(String s) {
+        StringBuilder rev = new StringBuilder();
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            if (isVowel(c)) {
+                rev.append(c);
+            }
+        }
+
+        int k = 0;
+        char[] ch = s.toCharArray();
+        for (int i = 0; i < ch.length; i++) {
+            if (isVowel(ch[i])) {
+                ch[i] = rev.charAt(k++);
+            }
+        }
+        
+        return new String(ch);
+    }
+
+    public boolean isVowel(char c) {
+        c = Character.toLowerCase(c);
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
+    public static void main(String[] args) {
+        reverseVowels r = new reverseVowels();
+        String val = "race car";
+        String reuslt = r.reverseVowels(val);
+        System.out.println(reuslt);
+    }
+}
