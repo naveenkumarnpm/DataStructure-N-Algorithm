bd6d87bde325d68b490c408c53f52a8dbcc19a30
naveenkumar <naveenkumarnb76@gmail.com>
2024-05-17 23:18:00 +0530

    solution to longest subset with zero sum

---
+++ Arrays/SolutLongestSubsetWithZeroSum.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.* ;
+
+import java.util.ArrayList;
+
+public class SolutLongestSubsetWithZeroSum {
+
+	public static int LongestSubsetWithZeroSumFuction(ArrayList<Integer> arr) {
+
+		// Write your code here.
+		return 1;
+		
+	}
+}
