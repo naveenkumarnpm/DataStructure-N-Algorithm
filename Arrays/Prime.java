4a1e5a2afa4b059b5f3aa9d212fc6188f71638b1
naveenkumar <naveenkumarnb76@gmail.com>
2024-05-28 23:20:13 +0530

    to find prime number

---
+++ Arrays/Prime.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Prime {
+    public static void main(String[] args) {
+        
+        Scanner sc= new Scanner(System.in);
+        int n=0;
+        System.out.println("enter to number to find a prime number");
+        n=sc.nextInt();
+
+        if(n<=1)
+        
+    }
+}
